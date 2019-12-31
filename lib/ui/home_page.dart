3394a9cae3e0bd0e869fa3b6d4ea2acfb06da03a
Werisu <wellysson35@gmail.com>
2019-12-31 15:49:51 -0300

    Grid de Gifs

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -56,9 +56,53 @@ class _HomePageState extends State<HomePage> {
               style: TextStyle(color: Colors.white, fontSize: 18.0),
               textAlign: TextAlign.center,
             ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: _getGifs(),
+              builder: (context, snapshot){
+                switch(snapshot.connectionState){
+                  case ConnectionState.waiting:
+
+                  case ConnectionState.none:
+                    return Container(
+                      width: 200.0,
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        strokeWidth: 5.0,
+                      ),
+                    );
+                  default:
+                    if(snapshot.hasError) return Container();
+                    else return _criarTabelaDeGif(context, snapshot);
+                }
+              },
+            ),
           )
         ],
       ),
+    );
+  }
+
+  Widget _criarTabelaDeGif(BuildContext context, AsyncSnapshot snapshot){
+    return GridView.builder(
+        padding: EdgeInsets.all(10.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0
+        ),
+        itemCount: snapshot.data["data"].length,
+        itemBuilder: (context, index){
+          /* GestureDetector para clicar na imagem */
+          return GestureDetector(
+            child: Image.network(snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+            height: 300.0,
+                fit:BoxFit.cover,),
+          );
+        }
     );
   }
 }
