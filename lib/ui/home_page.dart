4073f13c339d738c2d72ddaf7fd8194dfe697787
Werisu <wellysson35@gmail.com>
2019-12-31 16:44:31 -0300

    Compartilhando Gifs

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:enjurus_gifis/ui/gif_page.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -121,6 +122,9 @@ class _HomePageState extends State<HomePage> {
                     context,
                     MaterialPageRoute(builder: (context) => GifPage(snapshot.data["data"][index]))
                 );
+              },
+              onLongPress: (){
+                Share.share(snapshot.data["data"][index]["images"]["fixed_height"]["url"]);
               },
             );
             else
