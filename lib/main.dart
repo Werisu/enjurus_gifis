ffbc079b96d86cd0757ba1f3f840529eb86bf9ec
Werisu <wellysson35@gmail.com>
2019-12-31 14:10:54 -0300

    Inciaindo projeto

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:enjurus_gifis/ui/home_page.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    title: "Buscador de Gif",
+  ));
+}
---
+++ lib/ui/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  _getGifs() async{
+    http.Response response;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
